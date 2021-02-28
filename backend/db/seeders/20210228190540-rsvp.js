d45760afbb33d10fc6aaba5b188cf9fd124f44a4
Caleb <caleb-gilbert@live.com>
2021-02-28 14:46:30 -0500

    seeded data

--- backend/db/seeders/20210228190540-rsvp.js
+++ backend/db/seeders/20210228190540-rsvp.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Comments', [
+    return queryInterface.bulkInsert('RSVPs', [
       {
         userId: 1,
         eventId: 1,
