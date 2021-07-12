5c03eb899812526912a64f3f7c51d84a99dbe6fb
Caleb Gilbert <clutchcaleb@gmail.com>
2021-07-12 18:07:12 -0400

    commentHolder component made

---
+++ frontend/src/components/Comments/CommentHolder.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeComment, updateComment } from '../../store/comment';
+
+const CommentHolder = ({ comment, id }) => {
+  const dispatch = useDispatch();
+  const [commentEditor, setCommentEditor] = useState(false);
+  const [content, setContent] = useState('');
+
+  const userId = useSelector(state => state.session.user.id);
+
+	const editComment = async (comment, newComment, id) => {
+		if (newComment !== comment.content) {
+			console.log('in editComment()')
+		}
+		setCommentEditor(!commentEditor)
+	}
+
+	const EditComment = ({ func, comment }) => {
+		const [value, setValue] = useState(comment.content);
+		const keyPress = (e) => {
+			if (e.key === 'Enter') {
+				e.preventDefault();
+				func(value, comment.id)
+			}
+		};
+		return (
+			<div className="editCommentDiv">
+				<textarea
+					maxLength="140"
+					onChange={(e) => setValue(e.target.value)}
+					onKeyPress={keyPress}
+					value={value}
+					className="messageInputTextarea"
+				>
+					{comment.content}
+				</textarea>
+			</div>
+		)
+	}
+	
+	const deleteCommentId = (id) => {
+		dispatch(removeComment(id));
+	};
+
+  const keyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
+  }
+
+  return (
+    <>
+      <div key={comment?.id} className="comment">
+        {comment?.content}
+      </div>
+      {userId === comment.userId && (
+        <>
+          <button onClick={() => setCommentEditor(!commentEditor)}>edit.</button>
+          <button onClick={() => deleteCommentId(comment.id)}>delete.</button>
+        </>
+      )}
+      {commentEditor &&
+        <EditComment func={editComment} comment={comment} />
+      }
+    </>
+  )
+}
+
+
+export default CommentHolder;
--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getComment, createComment, removeComment } from '../../store/comment';
+import CommentHolder from './CommentHolder';
 
 const CommentSection = ({ id, userId }) => {
   const dispatch = useDispatch();
@@ -24,39 +25,6 @@ const CommentSection = ({ id, userId }) => {
 		setContent('');
 	};
 
-	const editComment = async (comment, newComment, id) => {
-		if (newComment !== comment.content) {
-			console.log('in editComment()')
-		}
-		setCommentEditor(!commentEditor)
-	}
-
-	const EditComment = ({ func, comment }) => {
-		const [value, setValue] = useState(comment.content);
-		const keyPress = (e) => {
-			if (e.key === 'Enter') {
-				e.preventDefault();
-				func(value, comment.id)
-			}
-		};
-		return (
-			<div className="editCommentDiv">
-				<textarea
-					maxLength="140"
-					onChange={(e) => setValue(e.target.value)}
-					onKeyPress={keyPress}
-					value={value}
-					className="messageInputTextarea"
-				>
-					{comment.content}
-				</textarea>
-			</div>
-		)
-	}
-	const deleteCommentId = (id) => {
-		dispatch(removeComment(id));
-	};
-
 	const keyPress = (e) => {
 		if (e.key === 'Enter') {
 			e.preventDefault();
@@ -73,20 +41,7 @@ const CommentSection = ({ id, userId }) => {
       <div className="comments">
         {commentsArray
           ?.map((comment) => (
-            <>
-              <div key={comment?.id} className="comment">
-                {comment?.content}
-              </div>
-              {userId === comment.userId && (
-                <>
-                  <button onClick={() => setCommentEditor(!commentEditor)}>edit.</button>
-                  <button onClick={() => deleteCommentId(comment.id)}>delete.</button>
-                </>
-              )}
-              {commentEditor &&
-                <EditComment func={editComment} comment={comment} />
-              }
-            </>
+						<CommentHolder comment = {comment} id = { id }key={comment.id} />
           ))}
       </div>
       <div>
