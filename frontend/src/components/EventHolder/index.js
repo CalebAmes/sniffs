c7107f72ed29961592161beb9f8338b176701034
Caleb Gilbert <clutchcaleb@gmail.com>
2021-07-13 19:04:38 -0400

    setTimeout

--- frontend/src/components/EventHolder/index.js
+++ frontend/src/components/EventHolder/index.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './EventHolder.css'
 
 const EventHolder = ({ event, i }) => {
+  const [increment, setIncrement] = useState(i);
+
+  useEffect(() => {
+    let timeout = setInterval(() => {
+      setIncrement(increment + 1);
+    }, 3000);
+    return () => clearInterval(timeout);
+  }, [increment]);
 
   return (
     <>
       <Link to={`/event/${event?.id}`} className='eventHolder'>
-        <img className='eventPhoto' src={`${ event?.photo[i % 3] }`} />
+        <img className='eventPhoto' src={`${ event?.photo[increment % 3] }`} />
         <div className='lower'>
           <h3>{ event?.name }</h3>
           <p>{event?.description}</p>
