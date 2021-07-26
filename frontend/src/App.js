d6bf38b25884cc254b899ec0eea0acdae159deb6
Caleb <caleb-gilbert@live.com>
2021-07-26 17:36:33 -0400

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm';
-import Navigation from './components/Navigation';
-import LandingPage from './components/LandingPage';
-import UserProfile from './components/UserProfile';
-import EventForm from './components/EventForm';
-import ProfileForm from './components/ProfileForm';
-import EventPage from './components/EventPage';
-import CategoryPage from './components/CategoryPage';
-import * as sessionActions from './store/session';
-import { getEvent } from './store/event';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import Navigation from "./components/Navigation";
+import LandingPage from "./components/LandingPage";
+import UserProfile from "./components/UserProfile";
+import EventForm from "./components/EventForm";
+import ProfileForm from "./components/ProfileForm";
+import EventPage from "./components/EventPage";
+import CategoryPage from "./components/CategoryPage";
+import Footer from "./components/Footer";
+import * as sessionActions from "./store/session";
+import { getEvent } from "./store/event";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
-    dispatch(getEvent()).then(() => {
-      dispatch(sessionActions.restoreUser())
-    }).then(() => setIsLoaded(true))
+    dispatch(getEvent())
+      .then(() => {
+        dispatch(sessionActions.restoreUser());
+      })
+      .then(() => setIsLoaded(true));
   }, [dispatch]);
   return (
     <>
-      <Navigation isLoaded={ isLoaded }/>
-      { !isLoaded &&
-        <div className='loading' />
-      }
-      { isLoaded && (
-      <Switch>
-        <Route exact path='/'>
-          <LandingPage />
-        </Route>
-        <Route path='/category/:id'>
-          <CategoryPage />
-        </Route>
-        <Route path='/event/:id'>
-          <EventPage />
-        </Route>
-        <Route path='/editProfile'>
-          <ProfileForm />
-        </Route>
-        <Route path='/createEvent'>
-          <EventForm />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-        <Route path='/login'>
-          <LoginForm />
-        </Route>
-        <Route path='/signup'>
-          <SignupForm />
-        </Route>
-        <Route>
-          <LandingPage/>
-        </Route>
-      </Switch>
+      <Navigation isLoaded={isLoaded} />
+      {!isLoaded && <div className="loading" />}
+      {isLoaded && (
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/category/:id">
+            <CategoryPage />
+          </Route>
+          <Route path="/event/:id">
+            <EventPage />
+          </Route>
+          <Route path="/editProfile">
+            <ProfileForm />
+          </Route>
+          <Route path="/createEvent">
+            <EventForm />
+          </Route>
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/signup">
+            <SignupForm />
+          </Route>
+          <Route>
+            <LandingPage />
+          </Route>
+        </Switch>
       )}
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,31 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footerDiv">
+      <a
+        className="footerItem"
+        href="https://github.com/CalebAmes"
+        target="_blank"
+      >
+        Github
+      </a>
+      <a
+        className="footerItem"
+        href="https://www.linkedin.com/in/caleb-gilbert-b522ab142/"
+        target="_blank"
+      >
+        LinkedIn
+      </a>
+      <a
+        className="footerItem"
+        href="https://calebames.github.io"
+        target="_blank"
+      >
+        Portfolio Site
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
