67c6f6e808f86ede73c327c79bb5974b5127b205
Caleb <caleb-gilbert@live.com>
2021-07-29 23:07:08 -0400

    avatar added to .toSafeObject

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -64,8 +64,8 @@ module.exports = (sequelize, DataTypes) => {
     User.hasMany(models.Comment, { foreignKey: 'userId'});
   };
   User.prototype.toSafeObject = function() {
-    const { id, username, email } = this;
-    return { id, username, email };
+    const { id, username, email, avatar } = this;
+    return { id, username, email, avatar };
   }
   User.prototype.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.hashedPassword.toString());
