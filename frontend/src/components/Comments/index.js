c5f3613c856249904da74b1e091de8ed6c9b865f
Caleb <caleb-gilbert@live.com>
2021-08-05 00:10:40 -0400

    scrollBottom and Animations

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -1,54 +1,63 @@
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router-dom';
-import { getComment, createComment } from '../../store/comment';
-import CommentHolder from './CommentHolder';
-import './comments.css'
+import { useHistory } from "react-router-dom";
+import { getComment, createComment } from "../../store/comment";
+import CommentHolder from "./CommentHolder";
+import "./comments.css";
 
 const CommentSection = ({ id, userId }) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const commentItems = useSelector((state) => state.comment);
-	const commentsArray = Object.values(commentItems).filter(comment =>
-			comment.eventId === parseInt(id))
+  const commentsArray = Object.values(commentItems).filter(
+    (comment) => comment.eventId === parseInt(id)
+  );
+  const commentBox = useRef();
 
+  const [content, setContent] = useState("");
 
-  const [content, setContent] = useState('');
+  const scroll2 = () => {
+    const commentBox = document.querySelector('.comments');
+    commentBox.scrollTop = commentBox.scrollHeight;
+  }
 
-  const addComment = (e) => {
-    if (!userId) return history.push('/login');
-		if (e) e.preventDefault();
-		dispatch(
-			createComment({
-				userId,
-				content,
-				eventId: id,
-			})
-		);
-		setContent('');
-	};
+  const addComment = async (e) => {
+    if (!userId) return history.push("/login");
+    if (e) e.preventDefault();
+    await dispatch(
+      createComment({
+        userId,
+        content,
+        eventId: id,
+      })
+    );
+    setContent("");
+    scroll2();
+  };
 
-	const keyPress = (e) => {
-		if (e.key === 'Enter') {
-			e.preventDefault();
-			addComment();
-		}
-	};
+  const keyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addComment();
+    }
+  };
 
-  useEffect(() => {
-    dispatch(getComment())
-  }, [dispatch])
+  useEffect(async () => {
+    const comments = commentBox.current;
+    const scroll = () => {
+      comments.scrollTop = comments.scrollHeight;
+    };
+    await dispatch(getComment());
+    scroll();
+  }, [dispatch]);
 
   return (
     <div className="commentBlock">
-      <div className="comments">
-        { commentsArray
-          ?.map((comment) => (
-						<CommentHolder comment = {comment} id = { id }key={comment.id} />
-          ))}
-        { !commentsArray.length &&
-          <h3>This is where you can post comments</h3>
-        }
+      <div className="comments" ref={commentBox}>
+        {commentsArray?.map((comment) => (
+          <CommentHolder comment={comment} id={id} key={comment.id} />
+        ))}
+        {!commentsArray.length && <h3>This is where you can post comments</h3>}
       </div>
       <div>
         <form value={userId} onSubmit={addComment}>
@@ -69,7 +78,7 @@ const CommentSection = ({ id, userId }) => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CommentSection
+export default CommentSection;
